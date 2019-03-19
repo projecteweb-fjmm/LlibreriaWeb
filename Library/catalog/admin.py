28bb91e9cfc3e24ce50b11070172994642840b8e
francesc <elskater98@gmail.com>
2019-03-19 10:35:44 +0100

    Initial Models

--- Library/catalog/admin.py
+++ Library/catalog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(Genre)
+admin.site.register(Book)
+admin.site.register(BookInstance)
+admin.site.register(Author)
--- Library/catalog/urls.py
+++ Library/catalog/urls.py
@@ -1,7 +1,3 @@
 from django.conf.urls import url
 from . import views
 from django.urls import path
-
-urlpatterns=[
-    #path('', views.homepage, name='homepage'),
-]
