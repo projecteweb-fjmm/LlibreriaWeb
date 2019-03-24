f67b9c82c02358b0dccd237582e032b30d649898
Francesc <elskater98@gmail.com>
2019-03-24 19:26:35 +0100

    Models: Genre, Author, Book : DONE & CHECKED

--- Library/catalog/admin.py
+++ Library/catalog/admin.py
@@ -4,6 +4,5 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Genre)
-admin.site.register(Book)
-admin.site.register(BookInstance)
 admin.site.register(Author)
+admin.site.register(Book)
--- Library/catalog/models.py
+++ Library/catalog/models.py
@@ -1,93 +1,42 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
 class Genre(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length = 64,help_text = "Escribe un genero literario e.j: Aventuras")
 
     def __str__(self):
         return self.name
 
-class Book(models.Model):
-    """
-    Modelo que representa un libro (pero no un Ejemplar específico).
-    """
-
-    title = models.CharField(max_length=200)
-
-    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
-    # ForeignKey, ya que un libro tiene un solo autor, pero el mismo autor puede haber escrito muchos libros.
-    # 'Author' es un string, en vez de un objeto, porque la clase Author aún no ha sido declarada.
-
-    summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
-
-    isbn = models.CharField('ISBN', max_length=13,
-                            help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
-
-    genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
-
-    # ManyToManyField, porque un género puede contener muchos libros y un libro puede cubrir varios géneros.
-    # La clase Genre ya ha sido definida, entonces podemos especificar el objeto arriba.
+class Author(models.Model):
+    first_name = models.CharField(max_length=32)
+    second_name = models.CharField(max_length=32)
 
     def __str__(self):
-        """
-        String que representa al objeto Book
-        """
-        return self.title
+        return '%s  %s' % (self.first_name,self.second_name)
+
+
+
+class Book (models.Model):
+
+    title = models.CharField( max_length= 128, help_text="Escribe el titulo del libro que desea guardar e.j: THE 100")
+
+    author = models.ForeignKey(Author,on_delete=models.SET_DEFAULT,default=1)
+
+    summary = models.TextField(max_length=500,help_text="Escribe un breve resumen del libro")
+
+    isb = models.CharField('ISBN',max_length=13,help_text="Escribe un ISBN valido (13 caracteres)")
+
+    genre = models.ManyToManyField(Genre,help_text="Selecciona un genero literario")
+
+    date_publicated = models.DateField(null=True,blank=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.title,self.author)
 
     def get_absolute_url(self):
-        """
-        Devuelve el URL a una instancia particular de Book
-        """
+
         return reverse('book-detail', args=[str(self.id)])
 
 
-import uuid  # Requerida para las instancias de libros únicos
-
-
-class BookInstance(models.Model):
-    """
-    Modelo que representa una copia específica de un libro (i.e. que puede ser prestado por la biblioteca).
-    """
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
-                          help_text="ID único para este libro particular en toda la biblioteca")
-    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
-    imprint = models.CharField(max_length=200)
-    due_back = models.DateField(null=True, blank=True)
-
-    LOAN_STATUS = (
-        ('m', 'Maintenance'),
-        ('o', 'On loan'),
-        ('a', 'Available'),
-        ('r', 'Reserved'),
-    )
-
-    status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m',
-                              help_text='Disponibilidad del libro')
-
-    class Meta:
-        ordering = ["due_back"]
-
-    def __str__(self):
-        """
-        String para representar el Objeto del Modelo
-        """
-        return '%s (%s)' % (self.id, self.book.title)
-
-class Author(models.Model):
-
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    date_of_birth = models.DateField(null=True, blank=True)
-    date_of_death = models.DateField('Died', null=True, blank=True)
-
-    def get_absolute_url(self):
-        """
-        Retorna la url para acceder a una instancia particular de un autor.
-        """
-        return reverse('author-detail', args=[str(self.id)])
-
-    def __str__(self):
-        """
-        String para representar el Objeto Modelo
-        """
-        return '%s, %s' % (self.last_name, self.first_name)
